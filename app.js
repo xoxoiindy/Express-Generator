c3e13a65efb46ee559c8a303114409a8b9e80cc7
Cindy <proanocindy@gmail.com>
2022-10-14 01:00:36 -0600

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const logger = require('morgan');
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -45,6 +47,9 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -59,9 +64,8 @@ function auth(req, res, next) {
       if (req.session.user === 'authenticated') {
           return next();
       } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
+         
+          return next();
       }
   }
 }
