74e719cb297af908e53ba85b25f2c7065faf9cc7
Cindy <proanocindy@gmail.com>
2022-10-19 02:42:33 -0600

    File Upload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsite', campsiteRouter);
 app.use('/promotion', promotionRouter);
 app.use('/partner', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
